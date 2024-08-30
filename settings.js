170101e52d88a9face526eb38355483653e04ecc
tanzeelmehar <47309799+tanzeelmehar@users.noreply.github.com>
2024-08-30 15:34:41 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "923187954845"
+global.ownername = "🐛Tech Help"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '923187954845'  //creator number
+global.ownername = '🐛Tech Help' //owner name
+global.botname = 'Tech Help ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
